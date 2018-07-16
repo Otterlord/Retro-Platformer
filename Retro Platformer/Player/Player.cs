087673ea681c8cf7c01bb4b7e1f27eb9bbfa2469
Otterlord <yourotterlord@gmail.com>
2018-07-15 17:11:59 -0700

    Before screwing everything up

--- Retro Platformer/Player/Player.cs
+++ Retro Platformer/Player/Player.cs
@@ -34,28 +34,85 @@ public class Player : KinematicBody2D
     private RayCast2D sensorL;
     private RayCast2D sensorR;
     private RayCast2D sensorC;
+    private CollisionShape2D hitBox;
+    private Node2D sensors;
     private Sprite sprite;
+    private int mode = 0;
+    private Vector2 offset = new Vector2(0, 30);
 
     private Vector2 floorNormal = new Vector2(0, -1);
 
     public override void _Ready() 
     {
+        sensors = (Node2D)GetNode("Sensors");
         sprite = (Sprite)GetNode("Sprite");
-        sensorL = (RayCast2D)GetNode("LSensor");
-        sensorR =  (RayCast2D)GetNode("RSensor");
-        sensorC = (RayCast2D)GetNode("CSensor");
+        sensorL = (RayCast2D)GetNode("Sensors/LSensor");
+        sensorR =  (RayCast2D)GetNode("Sensors/RSensor");
+        sensorC = (RayCast2D)GetNode("Sensors/CSensor");
+        hitBox = (CollisionShape2D)GetNode("HitBox");
+        sensorL.AddException(this);
+        sensorC.AddException(this);
+        sensorR.AddException(this);
     }
 
     public override void _Process(float delta) 
     {
         floorNormal = GetFloorNormal();
         rot = -floorNormal.AngleTo(new Vector2(0,-1));
-        //sensorC.Rotation = rot;
         if (sensorC.IsColliding()) sprite.Rotation = Mathf.Lerp(sprite.Rotation, -sensorC.GetCollisionNormal().AngleTo(new Vector2(0,-1)), rotSpd);
         else sprite.Rotation = Mathf.Lerp(sprite.Rotation, rot, rotSpd);
-        if (sensorC.IsColliding()) sprite.GlobalPosition = sensorC.GetCollisionPoint() - (new Vector2(0, 30));
-        //if (sensorL.IsColliding() || sensorR.IsColliding()) sprite.GlobalPosition = GetCollisionPoint() - new Vector2(0, 30);
+        if (sensorC.IsColliding()) sprite.GlobalPosition = sensorC.GetCollisionPoint() - (offset);
         else sprite.Position = Vector2.Zero;
+        float angle = Mathf.Rad2Deg(rot);
+        //GD.Print(Mathf.Rad2Deg(rot));
+        //mode = Mathf.RoundToInt(angle/90) % 4;
+         
+        if (mode == 0) {
+            if (angle > 45) mode = 1;
+            if (angle < -45) mode = 3;
+        }
+        else if (mode == 1) {
+            if (angle < 45) mode = 0;
+            if (angle > 95) mode = 2;
+        }
+        else if (mode == 2) {
+            if (angle < 90) mode = 1;
+            if (angle > -90) mode = 3;
+        }
+        else if (mode == 3) {
+            if (angle > -45) mode = 0;
+            if (angle < -95) mode = 2;
+        }
+        
+        //GD.Print(mode);
+        if (mode == 0) 
+        {
+            sensors.RotationDegrees = 0;
+            //RotationDegrees = 0;
+            hitBox.RotationDegrees = 0;
+            offset = new Vector2(0, 30);
+        }
+        else if (mode == 1) 
+        {
+            sensors.RotationDegrees = 90;
+            //RotationDegrees = 90;
+            hitBox.RotationDegrees = 90;
+            offset = new Vector2(-30, 0);
+        }
+        else if (mode == 2) 
+        {
+            sensors.RotationDegrees = 180;
+            //RotationDegrees = 180;
+            hitBox.RotationDegrees = 0;
+            offset = new Vector2(0, -30);
+        }
+        else 
+        {
+            sensors.RotationDegrees = -90;
+            //RotationDegrees = -90;
+            hitBox.RotationDegrees = -90;
+            offset = new Vector2(30, 0);
+        }
     }
 
     public override void _PhysicsProcess(float delta) 
@@ -102,12 +159,14 @@ public class Player : KinematicBody2D
         if (rot == 0) velocity.x += gsp;
         else 
         {
-            if (Mathf.Sign(rot) != Mathf.Sign(gsp)) velocity.x += gsp;
+            if (Mathf.Sign(rot) != Mathf.Sign(gsp)) 
+            {
+                velocity += new Vector2(gsp, 0).Rotated(rot);
+            }
             else velocity += new Vector2(gsp, 0).Rotated(rot);
         }
-
-        GD.Print(velocity);
         // Apply movement
+        GD.Print(velocity);
         MoveAndSlide(velocity, floorNormal);
     }
 
@@ -115,8 +174,22 @@ public class Player : KinematicBody2D
     {
         if (sensorL.IsColliding() && sensorR.IsColliding()) 
         {
-            if (sensorL.GetCollisionPoint().y >= sensorR.GetCollisionPoint().y) return sensorL.GetCollisionNormal();
-            else return sensorR.GetCollisionNormal();
+            if (mode == 0) {
+                if (sensorL.GetCollisionPoint().y >= sensorR.GetCollisionPoint().y) return sensorL.GetCollisionNormal();
+                else return sensorR.GetCollisionNormal();
+            }
+            else if (mode == 1) {
+                if (sensorL.GetCollisionPoint().x >= sensorR.GetCollisionPoint().x) return sensorL.GetCollisionNormal();
+                else return sensorR.GetCollisionNormal();
+            }
+            else if (mode == 2) {
+                if (sensorL.GetCollisionPoint().y <= sensorR.GetCollisionPoint().y) return sensorL.GetCollisionNormal();
+                else return sensorR.GetCollisionNormal();
+            }
+            else {
+                if (sensorL.GetCollisionPoint().x <= sensorR.GetCollisionPoint().x) return sensorL.GetCollisionNormal();
+                else return sensorR.GetCollisionNormal();
+            }
         }
         else if (sensorL.IsColliding()) return sensorL.GetCollisionNormal();
         else if (sensorR.IsColliding()) return sensorR.GetCollisionNormal();
@@ -158,7 +231,6 @@ public class Player : KinematicBody2D
 
     public void AirMove() 
     {
-        GD.Print("ho");
         if (Input.IsActionPressed("move_left")) 
         {
             gsp -= acc * 2;
